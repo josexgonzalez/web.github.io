6d1e6f52622e85c4fd5713a5bdc8176802d8bfa8
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 21:35:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -941,7 +941,7 @@ async function main(userlandRW, wkOnly = false) {
             is_elfldr_running = true;
         } else {
             await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 2000));
+            await new Promise(resolve => setTimeout(resolve, 1000));
         }
 
         if (await load_local_elf("etaHEN.bin") == 0) {
@@ -949,7 +949,7 @@ async function main(userlandRW, wkOnly = false) {
             is_etaHEN_running = true;
         } else {
             await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 2000));
+            await new Promise(resolve => setTimeout(resolve, 3000));
         }
 
         // const SOCK_NONBLOCK = 0x20000000; // for future reference, this is ignored if we're not jailbroken and explicitly setting it with fcntl returns SCE_KERNEL_ERROR_EACCES (at least on 4.03)
