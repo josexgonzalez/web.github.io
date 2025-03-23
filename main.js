cf8983d3d52c4984d4348df94671091caccd4886
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 03:14:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -676,14 +676,14 @@ async function main(userlandRW, wkOnly = false) {
         await log("We escaped now? in sandbox: " + is_in_sandbox, LogLevel.INFO);
 
         // Patch PS4 SDK version
-        if (typeof OFFSET_KERNEL_PS4SDK != 'undefined') {
-            await krw.write4(get_kaddr(OFFSET_KERNEL_PS4SDK), 0x99999999);
+        if (typeof OFFSET_KERNEL_DATA_BASE_PS4SDK != 'undefined') {
+            await krw.write4(get_kaddr(OFFSET_KERNEL_DATA_BASE_PS4SDK), 0x99999999);
             await log("Patched PS4 SDK version to 99.99", LogLevel.INFO);
         }
 
         // Patch PS5 SDK version
-        if (typeof OFFSET_KERNEL_PS5SDK != 'undefined') {
-            await krw.write4(get_kaddr(OFFSET_KERNEL_PS5SDK), 0x99999999);
+        if (typeof OFFSET_KERNEL_DATA_BASE_PS5SDK != 'undefined') {
+            await krw.write4(get_kaddr(OFFSET_KERNEL_DATA_BASE_PS5SDK), 0x99999999);
             await log("Patched PS5 SDK version to 99.99", LogLevel.INFO);
         }
 
