cfbda34414b6473f04470a2ae440b080da5cfa62
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 23:57:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -583,8 +583,8 @@ async function main(userlandRW, wkOnly = false) {
     populatePayloadsPage(wkOnly);
 
     var load_payload_into_elf_store_from_local_file = async function (filename) {
-        showTemporaryAlert("[+] Loading ELF file: " + filename + " ...", LogLevel.LOG);
         const response = await fetch('payloads/' + filename);
+        showTemporaryAlert("[+] Loading ELF file: " + filename + " ...", LogLevel.LOG);
         if (!response.ok) {
             throw new Error(`Failed to fetch the binary file. Status: ${response.status}`);
         }
@@ -936,21 +936,25 @@ async function main(userlandRW, wkOnly = false) {
             }
         }
 
-        if (await load_local_elf("elfldr.bin") == 0) {
-            await log(`elfldr listening on ${ip.ip}:9020`, LogLevel.INFO);
+        if (await load_local_elf("elfldr.elf") == 0) {
+            await log(`elfldr listening on ${ip.ip}:9021`, LogLevel.INFO);
             is_elfldr_running = true;
         } else {
             await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            await new Promise(resolve => setTimeout(resolve, 2000));
         }
 
-        if (await load_local_elf("etaHEN.bin") == 0) {
+        // Esperar 4 segundos antes de lanzar el siguiente payload
+        await new Promise(resolve => setTimeout(resolve, 4000));
+
+        if (await load_local_elf("etaHEN.elf") == 0) {
             await log(`etaHEN listening on ${ip.ip}:9021`, LogLevel.INFO);
             is_etaHEN_running = true;
         } else {
             await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 3000));
+            await new Promise(resolve => setTimeout(resolve, 9000));
         }
+
 
         // const SOCK_NONBLOCK = 0x20000000; // for future reference, this is ignored if we're not jailbroken and explicitly setting it with fcntl returns SCE_KERNEL_ERROR_EACCES (at least on 4.03)
 
