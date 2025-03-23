a4f42d0b01f6e0b2e3a6e2369bc1d82395f3b2e5
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 02:07:40 +0100

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,21 @@
+const payload_map =
+    [
+    {
+        displayTitle: 'etaHEN',
+        description: 'etaHEN',
+        fileName: 'etaHEN.bin',
+        author: '-',
+        source: '-', 
+        version: '-' 
+    }
+,
+    {
+        displayTitle: 'elfldr',
+        description: 'elfldr',
+        fileName: 'elfldr.bin',
+        author: '-',
+        source: '-', 
+        version: '-' 
+    }
+];
+
