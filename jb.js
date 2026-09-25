5bf5dd5f6f47e9db919b8008a754cd294530cab1
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2026-09-25 18:44:05 +0200

    Add files via upload

--- jb.js
+++ jb.js
@@ -18,7 +18,7 @@ function showTemporaryAlert(message, callback) {
 
     // Crear el elemento de la imagen
     let icon = document.createElement('img');
-    icon.src = '/icon.png'; // Cambia 'setting.png' al nombre de tu imagen en el directorio raíz
+    icon.src = 'https://github.com/josexgonzalez/web.github.io/blob/index.html/icon.png?raw=true'; // Cambia 'setting.png' al nombre de tu imagen en el directorio raíz
     icon.style.width = '44px'; // Tamaño del icono
     icon.style.height = '44px';
     icon.style.marginRight = '7px'; // Espacio entre el icono y el texto
